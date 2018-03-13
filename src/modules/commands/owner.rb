5f6f74312dc10a7c6f04fedf8b7cf02999308307
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-03-13 15:30:15 +0000

    m8

--- src/modules/commands/owner.rb
+++ src/modules/commands/owner.rb
@@ -4,7 +4,7 @@ module Bot::Commands
   module Owner
    extend Discordrb::Commands::CommandContainer
    command(:eval, help_available:false) do |event|
-    break unless event.user.id == Bot::CONFIG.ownerID
+    break unless event.user.id == Bot::CONFIG.ownerID.to_i
     begin
      event.respond "```rb\n#{eval event.message.content.gsub 'r.eval ', ''}"
      event.message.react "✅"
@@ -14,7 +14,7 @@ module Bot::Commands
    end
   end
   command(:update, help_available:false) do |event|
-   break unless event.user.id == Bot::CONFIG.ownerID
+   break unless event.user.id == Bot::CONFIG.ownerID.to_i
    begin
     exec "git pull && bundler exec ruby src/bot.rb"
     event.respond "Updated Successfully"
@@ -23,11 +23,12 @@ module Bot::Commands
    end
   end
   command(:shutdown, help_available: false) do |event|
-    break unless event.user.id == Bot::CONFIG.ownerID
+    break unless event.user.id == Bot::CONFIG.ownerID.to_i
     event.respond "Bot is shutting down..."
     exit
   end
   command(:restart, help_available: false) do |event|
+    break unless event.user.id == Bot::CONFIG.ownerID.to_i
     event.respond "Bot is restarting..."
     exec "bundler exec ruby src/bot.rb"
   end
