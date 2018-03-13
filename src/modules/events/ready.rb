3faa663d45fb59263bceab3c89b020dc3c709842
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-03-13 15:19:20 +0000

    maybe fix

--- src/modules/events/ready.rb
+++ src/modules/events/ready.rb
@@ -4,6 +4,7 @@ module Bot::Events
  module Ready
   extend Discordrb::EventContainer
    puts "Bot is online!"
+   sleep 3
    Bot::BOT.game = "r.help | #{Bot::BOT.servers.size} servers!"
  end
 end
